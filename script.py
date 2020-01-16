f3aff1a5f42cd811a41fc4c83794249e2628219a
markuskruse <markus.borbely@gmail.com>
2020-01-16 12:56:43 +0100

    Animated gif

--- script.py
+++ script.py
@@ -1,18 +1,43 @@
 import os, bpy, sys, glob, math
 
+from math import radians
+from PIL import Image, ImageDraw
+
 ########################################
 #
+# Mandatory parameters
 # set these paths and then click run script
+# examples for windows, linux and mac can be found below
 #
 ########################################
-source_dir = "C:/path-to/your/dir"
-target_dir = "C:/tmp/"
+
+#source_dir = "/home/user/librarian"
+#target_dir = "/home/user/librarian"
+source_dir = "C:/tmp/register/"
+target_dir = "C:/tmp/register/"
+
+########################################
+#
+# Parameters with sensible defaults
+#
+########################################
+
+width = 500
+height = 500
+anim_frames = 16
+anim_seconds = 3
+anim_scale = 50
+
+########################################
+#
+# program start
+#
+########################################
 
 #render settings
 scene = bpy.data.scenes["Scene"]
-scene.render.resolution_x = 500
-scene.render.resolution_y = 500
-scene.render.resolution_percentage = 100
+scene.render.resolution_x = width
+scene.render.resolution_y = height
 scene.render.image_settings.file_format='JPEG'
 
 #this script two materials to be present in the scene
@@ -37,7 +62,11 @@ for i, fp in enumerate(files):
         bpy.ops.import_mesh.stl(filepath=fp)
         ob = bpy.context.active_object
         modelname = ob.name
-        
+
+        #get center of object
+        bpy.ops.object.origin_set(type='ORIGIN_CENTER_OF_MASS', center='MEDIAN')
+        ob.location = (0, 0, 0)
+                        
         #set material
         ob.data.materials.append(mat)
 
@@ -45,7 +74,7 @@ for i, fp in enumerate(files):
         bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY')
         x = ob.location.x
         y = ob.location.y
-        z = ob.location.z + ob.dimensions.z / 1.5
+        z = ob.location.z + max(ob.dimensions.z / 1.2, ob.dimensions.x / 2, ob.dimensions.y / 2)
 
         #create label
         bpy.ops.object.text_add(location=(x,y,z), radius=1)
@@ -61,11 +90,8 @@ for i, fp in enumerate(files):
         
         #scale text to match model
         obj_size = math.sqrt(ob.dimensions[0]**2 + ob.dimensions[1]**2 + ob.dimensions[2]**2)
-        print (obj_size)
         text_size = math.sqrt(textobject.dimensions[0]**2 + textobject.dimensions[1]**2 + textobject.dimensions[2]**2)
-        print (text_size)
         scale = obj_size / (text_size * 2)
-        print ("scale", scale)
         textobject.scale = (scale, scale, scale)
 
         #text material
@@ -74,22 +100,87 @@ for i, fp in enumerate(files):
         #focus camera on object
         ob.select_set(True)
         textobject.select_set(True)
-        scene.camera.data.angle = 45*(pi/180.0)
+        scene.camera.data.angle = radians(45)
         bpy.ops.view3d.camera_to_view_selected()
-        scene.camera.data.angle = 50*(pi/180.0)
+        scene.camera.data.angle = radians(50)
         
         #set render properties and render
+        scene.render.resolution_percentage = 100
         bpy.context.scene.render.filepath = target_dir + modelname + ".jpg"
         bpy.ops.render.render(use_viewport = True, write_still=True)
-        print("STL rendered: ", ob.name)
-        images.append(modelname + ".jpg")
+        images.append(modelname)
+
+        #Delete text
+        ob.select_set(False)
+        bpy.ops.object.delete() 
+        ob.select_set(True)
+        
+        #reposition camera
+        scene.camera.data.angle = radians(45)
+        bpy.ops.view3d.camera_to_view_selected()
+        scene.camera.data.angle = radians(50)
+        loc = scene.camera.location
+        camera_z = scene.camera.location[2]
+        distance = math.sqrt(loc[0] * loc[0] + loc[1] * loc[1])
+
+        animnames = []
+        for index in range(anim_frames):
+            # rotate camera based on angle
+            angle = index * (360 / anim_frames)
+            scene.camera.rotation_euler = (radians(45), 0.0, radians(angle))
+            scene.camera.location = (math.sin(radians(angle)) * distance, -1 * math.cos(radians(angle)) * distance, camera_z)
+            
+            #set render properties and render
+            scene.render.resolution_percentage = anim_scale
+            filename = target_dir + "frame_" + str(index) + ".jpg"
+            animnames.append(filename)
+            bpy.context.scene.render.filepath = filename
+            bpy.ops.render.render(use_viewport = True, write_still=True)
+
+        anim = []
+        for index in range(anim_frames):
+            im = Image.open(animnames[index])
+            anim.append(im)
+            
+        gifim = anim[0]
+        gifim.save(target_dir + modelname + "_anim.gif", save_all=True, optimize=True, append_images=anim[1:anim_frames], duration=anim_seconds/anim_frames, loop=0)
 
         #delete object
         bpy.ops.object.delete() 
 
+html_top = """
+<!doctype html>
+
+<html>
+    <head>
+        <style>"""
+
+css_template= """
+        div.img{0} {{
+            background-image: url('{1}.jpg');
+            width: {2}px;
+            height: {3}px;
+            display: inline-block;
+            border: 1px solid black;
+        }}
+        div.img{0}:hover {{
+            background-image: url('{1}_anim.gif');
+            background-size: contain;
+        }}"""
+
+image_template = """
+<div class="img{0}" title="{1}">&nbsp;</div>"""
+
 with open(target_dir + "stl-model-register.html", "w") as file:
-    file.write("<html><body>")
+    file.write(html_top)
     for i, image in enumerate(images):
-        print("Image: ", image)
-        file.write("<img src=\"%s\" alt=\"%s\">\n" % (image, image))
+        file.write(css_template.format(i, image, width, height))
+    file.write("</style></head><body>")
+    for i, image in enumerate(images):
+        file.write(image_template.format(i, image))
     file.write("</body></html>")
+    
+for index in range(anim_frames):
+    os.remove(animnames[index])
+
+print("Ook")
